43579019774a5784527bfdacf3ff593eb2e7f22a
patrickkuna21-prog <patrickkuna21@gmail.com>
2025-11-09 17:03:48 +0100

    first commit

--- Mirroredtriangle.py
+++ Mirroredtriangle.py
@@ -1,5 +1,3 @@
-total_rows=5
-for row_number in range(1,total_rows+1):
-    spaces=total_rows - row_number
-    stars=row_number
-    print(""* spaces +"*"*row_number)
+n=5
+for i in range(1, n+1):
+    print(" " * (n - i) + "*" * i) 
